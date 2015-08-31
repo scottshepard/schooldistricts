727d2ae9cf0622e03733f5939ec302f3ddac6ec2
scotts <scotts@esparklearning.com>
2015-08-31 13:31:08 -0500

    file to add datasets and docs to package source

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' General information and location for 17,889 U.S. school districts
+#'
+"publicschooldistricts"
+
+#' General information and location for 101,385 U.S. public schools
+#'
+"publicschools"
+
+#' General information and location for 28,347 U.S. private schools
+#'
+"privateschools"
