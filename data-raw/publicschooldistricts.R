8f91fcb04090f36f9a38f48fa8b60cae45e44acd
scotts <scotts@esparklearning.com>
2015-08-31 15:45:20 -0500

    Bug fix in public school districts data so that lat and long are saved as numerics instead of factors

--- data-raw/publicschooldistricts.R
+++ data-raw/publicschooldistricts.R
@@ -1,2 +1,4 @@
 publicschooldistricts <- read.csv("data-raw/publicschooldistricts_1.0.5.csv")
+publicschooldistricts$loc_LAT_centroid <- as.numeric(as.character(publicschooldistricts$loc_LAT_centroid))
+
 save(publicschooldistricts, file="data/publicschooldistricts.rdata")
